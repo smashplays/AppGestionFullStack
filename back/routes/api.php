1a456e9f22f3eb693f9c3c1505b9f112174a4b47
Jesús Terrés Hernández <86616945+smashplays@users.noreply.github.com>
2023-05-30 13:47:21 +0200

    Update api.php
    Protecting routes with login middleware

--- back/routes/api.php
+++ back/routes/api.php
@@ -19,48 +19,48 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('/clients')->group(function () {
-    Route::get('', [ClientController::class, 'getAll']);
-    Route::middleware('validate.id')->get('/{id}', [ClientController::class, 'getById']);
-    Route::post('', [ClientController::class, 'post']);
-    Route::middleware('validate.id')->delete('/{id}', [ClientController::class, 'delete']);
-    Route::middleware('validate.id')->patch('/{id}', [ClientController::class, 'update']);
-    Route::middleware('validate.id')->get('/{id}/documents', [ClientController::class, 'documents']);
-    Route::middleware('validate.id')->get('/{id}/terminados', [ClientController::class, 'terminados']);
+    Route::middleware('login')->get('', [ClientController::class, 'getAll']);
+    Route::middleware('validate.id', 'login')->get('/{id}', [ClientController::class, 'getById']);
+    Route::middleware('login')->post('', [ClientController::class, 'post']);
+    Route::middleware('validate.id', 'login')->delete('/{id}', [ClientController::class, 'delete']);
+    Route::middleware('validate.id', 'login')->patch('/{id}', [ClientController::class, 'update']);
+    Route::middleware('validate.id', 'login')->get('/{id}/documents', [ClientController::class, 'documents']);
+    Route::middleware('validate.id', 'login')->get('/{id}/terminados', [ClientController::class, 'terminados']);
 });
 
 Route::prefix('/documents')->group(function () {
-    Route::get('', [DocumentController::class, 'getAll']);
-    Route::middleware('validate.id')->get('/{id}', [DocumentController::class, 'getById']);
-    Route::post('', [DocumentController::class, 'post']);
-    Route::middleware('validate.id')->delete('/{id}', [DocumentController::class, 'delete']);
-    Route::middleware('validate.id')->patch('/{id}', [DocumentController::class, 'update']);
-    Route::middleware('validate.id')->get('/{id}/client', [DocumentController::class, 'client']);
+    Route::middleware('login')->get('', [DocumentController::class, 'getAll']);
+    Route::middleware('validate.id', 'login')->get('/{id}', [DocumentController::class, 'getById']);
+    Route::middleware('login')->post('', [DocumentController::class, 'post']);
+    Route::middleware('validate.id', 'login')->delete('/{id}', [DocumentController::class, 'delete']);
+    Route::middleware('validate.id', 'login')->patch('/{id}', [DocumentController::class, 'update']);
+    Route::middleware('validate.id', 'login')->get('/{id}/client', [DocumentController::class, 'client']);
 });
 
 Route::prefix('/terminados')->group(function () {
-    Route::get('', [TerminadoController::class, 'getAll']);
-    Route::middleware('validate.id')->get('/{id}', [TerminadoController::class, 'getById']);
-    Route::post('', [TerminadoController::class, 'post']);
-    Route::middleware('validate.id')->delete('/{id}', [TerminadoController::class, 'delete']);
-    Route::middleware('validate.id')->patch('/{id}', [TerminadoController::class, 'update']);
-    Route::middleware('validate.id')->get('/{id}/client', [TerminadoController::class, 'client']);
+    Route::middleware('login')->get('', [TerminadoController::class, 'getAll']);
+    Route::middleware('validate.id', 'login')->get('/{id}', [TerminadoController::class, 'getById']);
+    Route::middleware('login')->post('', [TerminadoController::class, 'post']);
+    Route::middleware('validate.id', 'login')->delete('/{id}', [TerminadoController::class, 'delete']);
+    Route::middleware('validate.id', 'login')->patch('/{id}', [TerminadoController::class, 'update']);
+    Route::middleware('validate.id', 'login')->get('/{id}/client', [TerminadoController::class, 'client']);
 });
 
 Route::prefix('/users')->group(function () {
-    Route::get('', [LoginController::class, 'getUsers']);
+    Route::middleware('login')->get('', [LoginController::class, 'getUsers']);
     Route::middleware('login')->get('/logout', [LoginController::class, 'logout']);
     Route::middleware('login')->get('/me', [LoginController::class, 'whoAmI']);
-    Route::middleware('validate.id')->get('/{id}', [LoginController::class, 'getById']);
-    Route::middleware('validate.id')->patch('/{id}', [LoginController::class, 'updateUser']);
-    Route::middleware('validate.id')->delete('/{id}', [LoginController::class, 'deleteUser']);
+    Route::middleware('validate.id', 'login')->get('/{id}', [LoginController::class, 'getById']);
+    Route::middleware('validate.id', 'login')->patch('/{id}', [LoginController::class, 'updateUser']);
+    Route::middleware('validate.id', 'login')->delete('/{id}', [LoginController::class, 'deleteUser']);
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/register', [LoginController::class, 'crearUser']);
 });
 
 Route::prefix('/events')->group(function () {
-    Route::get('', [EventController::class, 'getAll']);
-    Route::middleware('validate.id')->get('/{id}', [EventController::class, 'getById']);
-    Route::post('', [EventController::class, 'post']);
-    Route::middleware('validate.id')->delete('/{id}', [EventController::class, 'delete']);
-    Route::middleware('validate.id')->patch('/{id}', [EventController::class, 'update']);
+    Route::middleware('login')->get('', [EventController::class, 'getAll']);
+    Route::middleware('validate.id', 'login')->get('/{id}', [EventController::class, 'getById']);
+    Route::middleware('login')->post('', [EventController::class, 'post']);
+    Route::middleware('validate.id', 'login')->delete('/{id}', [EventController::class, 'delete']);
+    Route::middleware('validate.id', 'login')->patch('/{id}', [EventController::class, 'update']);
 });
